7bb3324df69449321375682b8f0d384e46d96161
applycertificateglobal <applycertificateglobal@gmail.com>
2025-06-28 16:36:15 +0400

    Update App.js
    Update App.js with full Phase 1 layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Hero from './components/Hero';
 import BookingForm from './components/BookingForm';
-import Ambassador from './components/Ambassador';
+import AmbassadorSection from './components/AmbassadorSection';
+import FallbackCTA from './components/FallbackCTA';
+import InstagramEmbed from './components/InstagramEmbed';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="font-sans text-gray-800">
       <Hero />
-      <BookingForm />
-      <Ambassador />
+      <div className="max-w-6xl mx-auto px-4">
+        <BookingForm />
+        <AmbassadorSection />
+        <FallbackCTA />
+        <InstagramEmbed />
+      </div>
       <Footer />
     </div>
   );
