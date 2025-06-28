3b5b3bdb93c8eca216922fbae4c1d03df7b3a1eb
applycertificateglobal <applycertificateglobal@gmail.com>
2025-06-28 17:20:05 +0400

    Update and rename Footer.js to FooterSection.js

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Footer = () => (
+const FooterSection = () => (
   <footer className="text-center p-4 bg-gray-800 text-white mt-10">
     &copy; 2025 MyCampusVibe. All rights reserved.
   </footer>
 );
 
-export default Footer;
+export default FooterSection;
