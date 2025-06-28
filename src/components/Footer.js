d0fb4493ab03022e3118619f113a83940e1ac035
applycertificateglobal <applycertificateglobal@gmail.com>
2025-06-28 15:35:01 +0400

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="text-center p-4 bg-gray-800 text-white mt-10">
+    &copy; 2025 MyCampusVibe. All rights reserved.
+  </footer>
+);
+
+export default Footer;
