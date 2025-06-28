49c40830565c6b9162bd6699ad57c3ad816f3a96
applycertificateglobal <applycertificateglobal@gmail.com>
2025-06-28 15:41:37 +0400

    Create App.js
    Integrate all components in App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Hero from './components/Hero';
+import BookingForm from './components/BookingForm';
+import Ambassador from './components/Ambassador';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <BookingForm />
+      <Ambassador />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
